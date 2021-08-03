b6dca6c11d37252305a81c739e70666bb6c2b11c
Nickeron-dev <koshkin061969@gmail.com>
2021-08-03 23:19:49 +0300

    Configured spring security so it allows entering certain pages.

--- Exhibitions/src/main/java/com/project/exhibitions/SecurityConfig.java
+++ Exhibitions/src/main/java/com/project/exhibitions/SecurityConfig.java
@@ -4,12 +4,25 @@ import com.project.exhibitions.entity.RoleType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
     @Autowired
     public void globalConfig(AuthenticationManagerBuilder builder) throws Exception {
         builder.inMemoryAuthentication().withUser("user").password("password").roles("USER");
