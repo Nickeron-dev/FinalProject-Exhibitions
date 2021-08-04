8321e5fc0e17bcfecb3ecfa690f31176cf1e5b99
Nickeron-dev <koshkin061969@gmail.com>
2021-08-04 23:18:02 +0300

    Started rebuilding my project to add spring security properly.

---
+++ Exhibitions/src/main/java/com/project/exhibitions/services/UserService.java
@@ -0,0 +1,13 @@
+package com.project.exhibitions.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
