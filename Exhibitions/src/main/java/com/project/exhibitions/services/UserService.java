d9df850a3216bf3192c4e035dfe2a30af9e218ae
Nickeron-dev <koshkin061969@gmail.com>
2021-08-05 15:28:26 +0300

    Solved error that I had.

--- Exhibitions/src/main/java/com/project/exhibitions/services/UserService.java
+++ Exhibitions/src/main/java/com/project/exhibitions/services/UserService.java
@@ -5,9 +5,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements UserDetailsService {
 
     @Override
