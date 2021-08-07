4c1b05a200443c008dcfa12c991a7bedb380af07
Nickeron-dev <koshkin061969@gmail.com>
2021-08-07 13:32:57 +0300

    Removed files that I don't need and switched to another db.

--- Exhibitions/src/main/java/com/project/exhibitions/entity/User.java
+++ Exhibitions/src/main/java/com/project/exhibitions/entity/User.java
@@ -7,11 +7,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.List;
+
 
 @Data
 @Builder
