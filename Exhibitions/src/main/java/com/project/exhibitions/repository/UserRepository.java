5ff477c70b00d3682ba25fbc72dea9df45fa1594
Nickeron-dev <koshkin061969@gmail.com>
2021-08-07 13:22:23 +0300

    Changed way I accessed database and resolved error with creating bean of my repository.

--- Exhibitions/src/main/java/com/project/exhibitions/repository/UserRepository.java
+++ Exhibitions/src/main/java/com/project/exhibitions/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.project.exhibitions.repository;
 
 import com.project.exhibitions.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@NoRepositoryBean
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 }
--- Exhibitions/src/main/java/com/project/exhibitions/services/UserService.java
+++ Exhibitions/src/main/java/com/project/exhibitions/services/UserService.java
@@ -1,8 +1,9 @@
 package com.project.exhibitions.services;
 
-import com.project.exhibitions.dao.UserDAO;
 import com.project.exhibitions.entity.Role;
 import com.project.exhibitions.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,11 +15,11 @@ import java.util.List;
 @Service
 public class UserService implements UserDetailsService {
 
-    private UserDAO userDAO;
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userDAO.findByUsername(username).get();
+        return userRepository.findByUsername(username);
 
     }
 }
