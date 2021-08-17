aa1429c9810871efb3a760ede779820a77195a73
Nickeron-dev <koshkin061969@gmail.com>
2021-08-17 23:53:56 +0300

    Added TicketDTO.

---
+++ Exhibitions/src/main/java/com/project/exhibitions/dto/TicketDTO.java
@@ -0,0 +1,15 @@
+package com.project.exhibitions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TicketDTO {
+    private Integer userId;
+    private Integer exhibitionId;
+}
--- Exhibitions/src/main/java/com/project/exhibitions/entity/Ticket.java
+++ Exhibitions/src/main/java/com/project/exhibitions/entity/Ticket.java
@@ -1,6 +1,7 @@
 package com.project.exhibitions.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 
 @Entity
 @Table(name = "tickets")
