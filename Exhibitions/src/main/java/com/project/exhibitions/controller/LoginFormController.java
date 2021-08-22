371b63716637ded49faca48c93bbeede0aa8f55e
Nickeron-dev <koshkin061969@gmail.com>
2021-08-22 17:44:13 +0300

    Finished implementing logger.

--- Exhibitions/src/main/java/com/project/exhibitions/controller/LoginFormController.java
+++ Exhibitions/src/main/java/com/project/exhibitions/controller/LoginFormController.java
@@ -10,23 +10,23 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 
 @Slf4j
 @RestController
 @RequestMapping("/")
+@ResponseBody
 public class LoginFormController {
 
     private final UserService userService;
 
     @PostMapping("/login")
+    @ResponseBody
     public String loginUser(UserDTO user) throws UsernameNotFoundException {
         userService.loadUserByUsername(user.getUsername());
+        System.out.println("Hello");
         log.info("User logged in");
         return "login";
     }
