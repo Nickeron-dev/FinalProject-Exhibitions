6da8006e8062224935fdc867bb73a19ab8c04451
Nickeron-dev <koshkin061969@gmail.com>
2021-09-04 19:53:39 +0300

    Generated JavaDoc.

--- Exhibitions/src/main/java/com/project/exhibitions/repository/ExhibitionRepositoryImpl.java
+++ Exhibitions/src/main/java/com/project/exhibitions/repository/ExhibitionRepositoryImpl.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
